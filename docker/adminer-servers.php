d728c008bb7aeefc979eb57ba110520f58ad66e3
Ivan Yashin <newparadigma1@gmail.com>
2024-05-14 20:01:16 +0700

    Update admirer and readme. (#5)
    Update adminer service.
    Update readme.
    Remove DTO contract.

---
+++ docker/adminer-servers.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'db' => [
+        // Required parameters
+        'username'  => 'example',
+        'pass'      => 'example',
+        // Optional parameters
+        'driver'    => 'pgsql',
+        'label'     => 'example',
+        'databases' => [
+            'example' => 'example',
+        ]
+    ]
+];
